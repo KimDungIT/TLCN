f7554fd022c9fb0ce366952db59b1d4043493036
=KimDungIT <=>
2019-10-14 22:54:27 +0700

    Flyway

---
+++ src/main/resources/db/migration/V1__14102019_database.sql
@@ -0,0 +1,91 @@
+create table roles
+(
+    id_role   int auto_increment
+        primary key,
+    role_name varchar(255) not null
+);
+
+create table tutors
+(
+    id_tutor           int auto_increment
+        primary key,
+    classes            text         not null,
+    college            varchar(255) not null,
+    district_can_teach varchar(255) not null,
+    gender             varchar(11)  not null,
+    graduation_year    varchar(11)  not null,
+    image              varchar(255) null,
+    major              varchar(255) not null,
+    more_info          longtext     null,
+    status             varchar(255) not null,
+    subjects           text         not null,
+    year_of_birth      varchar(11)  not null
+);
+
+create table users
+(
+    id_user  int auto_increment
+        primary key,
+    address  varchar(255) not null,
+    email    varchar(255) null,
+    name     varchar(255) not null,
+    password varchar(100) not null,
+    phone    varchar(11)  not null,
+    constraint UK_du5v5sr43g5bfnji4vb8hg5s3
+        unique (phone)
+);
+
+create table classes
+(
+    id_class           int auto_increment
+        primary key,
+    address            varchar(255) not null,
+    gender_requirement varchar(255) null,
+    grade              varchar(255) not null,
+    level_requirement  varchar(255) null,
+    salary             double       not null,
+    service_fee        double       not null,
+    status             varchar(255) not null,
+    subject            varchar(255) not null,
+    time_teach         varchar(255) not null,
+    id_parent          int          not null,
+    constraint FK4yjsocmaeowccpdawmsfa00xc
+        foreign key (id_parent) references users (id_user)
+);
+
+create table class_tutor
+(
+    id_class int not null,
+    id_tutor int not null,
+    primary key (id_class, id_tutor),
+    constraint FKbv62sr46cx2urv7ke76cjnqug
+        foreign key (id_class) references classes (id_class),
+    constraint FKi9xf69k8mony38sj6nxvb0adr
+        foreign key (id_tutor) references tutors (id_tutor)
+);
+
+create table invoice
+(
+    id_invoice  int auto_increment
+        primary key,
+    service_fee double   not null,
+    time        datetime null,
+    id_class    int      not null,
+    id_tutor    int      not null,
+    constraint FK50jvq2a29cqgmar2n68806jm
+        foreign key (id_tutor) references tutors (id_tutor),
+    constraint FK6b18n5dwe2psjol4ant2uf84v
+        foreign key (id_class) references classes (id_class)
+);
+
+create table user_role
+(
+    id_user int not null,
+    id_role int not null,
+    primary key (id_user, id_role),
+    constraint FK2yqlxhjhgilevh7qvt2ve6udh
+        foreign key (id_role) references roles (id_role),
+    constraint FKr53t650tbjk5yipcm228wf1nc
+        foreign key (id_user) references users (id_user)
+);
+
---
+++ src/main/resources/db/migration/V2__14102019_adddata.sql
@@ -0,0 +1,7 @@
+insert into users (address, email, name, password, phone)
+values ('Quan 1', 'dung@gmail.com', 'dung', '$2a$10$C2.7wDGm8gTsU6lYA35fleShMzX7LHZO2I1.VOsGzE/h7GZdjxqg2', '0358143280');
+
+insert into roles (role_name) values ('GUEST');
+insert into roles (role_name) VALUES ('GIASU');
+insert into roles (role_name) VALUES ('PHUHUYNH');
+insert into roles (role_name) VALUES ('ADMIN');
