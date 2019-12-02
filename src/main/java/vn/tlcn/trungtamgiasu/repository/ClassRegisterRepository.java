e058aec2660f26679825c3a944d98a1d952fb3af
KimDungIT <kimdungit98@email.com>
2019-12-02 19:22:53 +0700

    sua getAllInfo

--- src/main/java/vn/tlcn/trungtamgiasu/repository/ClassRegisterRepository.java
+++ src/main/java/vn/tlcn/trungtamgiasu/repository/ClassRegisterRepository.java
@@ -20,6 +20,6 @@ public interface ClassRegisterRepository extends JpaRepository<ClassRegister, In
     @Query(value = "select t.image, u.name, t.level, c.payments, c.status\n" +
             "from class_register c join tutors t on c.id_tutor = t.id_tutor join users u on t.id_user = u.id_user\n" +
             "where c.id_class=?1", nativeQuery = true)
-    List<TutorRegisterDto> getAllInfo(int idClass);
+    List<String> getAllInfo(int idClass);
 
 }
