328254e6ecf8e00f7cc07e5a4820d68dd7abde84
KimDungIT <kimdungit98@email.com>
2020-01-03 18:02:21 +0700

    update db-migration

--- src/main/resources/db/migration/V3__27122019_addUser.sql
+++
@@ -1,2 +0,0 @@
-insert into users (address, email, name, password, phone)
-values ('Quận Thủ Đức', 'admin@gmail.com', 'admin', '$2a$10$.HjolvzOdkTnZGBTN5mc4e7ZUvt3L3IkQO71ZFX/sI9VmCI5oVznm', '0941672590');
